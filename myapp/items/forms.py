0c28196e33c5ddbac03d3d67eb1ff6326a847f57
Haziq Naeem <haziqnaeem36@gmail.com>
2022-04-11 14:34:07 -0400

    ItemForm create fields for name, desc, price, condition, size, contactInfo, category

--- myapp/items/forms.py
+++ myapp/items/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, SelectField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField, FloatField
 from wtforms.validators import DataRequired
 
 class ItemForm(FlaskForm):
     itemName = StringField('Item Name', validators=[DataRequired()])
     itemDescription = TextAreaField('Item Description', validators=[DataRequired()])
+    price = FloatField('Price', validators=[DataRequired()])
+    condition = TextAreaField('Condition', validators=[DataRequired()])
+    size = SelectField('Size', choices=[('XS'), ('S'), ('M'), ('L'), ('XL')])
+    contactInfo = TextAreaField('Contact Info', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Mens Clothes'), ('Womens Clothes'), ('Appliances'), ('Other')])
     submit = SubmitField('Post')
