7077868192bd8aa450b5344eb43a711111c47799
Haziq Naeem <haziqnaeem36@gmail.com>
2022-04-11 14:50:00 -0400

    items/views create route

--- myapp/items/views.py
+++ myapp/items/views.py
@@ -5,3 +5,16 @@ from myapp.models import Item
 from myapp.items.forms import ItemForm
 
 items = Blueprint('items', __name__)
+
+@items.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_item():
+  form = ItemForm()
+  if form.validate_on_submit():
+    item = Item(itemName=form.itemName.data, itemDescription=form.itemDescription.data, price=form.price.data, condition=form.condition.data, size=form.size.data, contactInfo=form.contactInfo.data, category=form.category.data)
+    db.session.add(item)
+    db.session.commit()
+    flash('ITEM CREATED') 
+    print('Item was created')
+    return redirect(url_for('core.index'))
+  return render_template('create_item.html', form=form)
