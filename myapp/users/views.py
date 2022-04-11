937614d1a4a5772b3d4244d837c857cb388f8e05
Haziq Naeem <haziqnaeem36@gmail.com>
2022-04-10 23:52:07 -0400

    add country to views.py

--- myapp/users/views.py
+++ myapp/users/views.py
@@ -14,7 +14,7 @@ users = Blueprint('users', __name__) # dont forget to register this in __init__.
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        user = User(email=form.email.data, username=form.username.data, country=form.country.data, password=form.password.data)
 
         db.session.add(user)
         db.session.commit()
