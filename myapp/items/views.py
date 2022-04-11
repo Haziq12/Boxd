455f30b13fadec7b20e9ba97277f8c5ca5057218
Haziq Naeem <haziqnaeem36@gmail.com>
2022-04-11 15:47:04 -0400

    update route and controller for item

--- myapp/items/views.py
+++ myapp/items/views.py
@@ -24,3 +24,35 @@ def item(item_id):
   item = Item.query.get_or_404(item_id)
   return render_template('item.html', date=item.date, itemName=item.itemName, itemDescription=item.itemDescription, price=item.price, condition=item.condition, size=item.size, contactInfo=item.contactInfo, category=item.category)
 
+@items.route('/<int:item_id>/update', methods=['GET', 'POST'])
+@login_required
+def update(item_id):
+  item = Item.query.get_or_404(item_id)
+
+  if item.author != current_user:
+    abort(403)
+
+  form = ItemForm()
+
+  if form.validate_on_submit():
+    item.itemName=form.itemName.data
+    item.itemDescription=form.itemDescription.data
+    item.price=form.price.data
+    item.condition=form.condition.data
+    item.size=form.size.data
+    item.contactInfo=form.contactInfo.data
+    item.category=form.category.data
+    db.session.commit()
+    flash('Item Updated')
+    return redirect(url_for('items.item', item_id=item.id))
+
+  elif request.method == 'GET':
+    form.itemName.data=item.itemName
+    form.itemDescription.data=item.itemDescription
+    form.price.data=item.price
+    form.condition.data=item.condition
+    form.size.data=item.size
+    form.contactInfo.data=item.contactInfo
+    form.category.data=item.category
+
+  return render_template('create_item.html', title='Updating', form=form) 
